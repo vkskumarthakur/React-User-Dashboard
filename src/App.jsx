c4195952dd4b5293e5204c3385a30e35bacb0739
vikas <vkskumarthakur@gmail.com>
2024-07-29 00:25:48 +0530

    adding codebase to remote

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'; // Add this line
+
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+   <div className='flex flex-col justify-center items-center bg-gray-900 min-h-screen'>
+    <h1 className="text-3xl text-white">
+     User Dashboard Coming Soon...😊
+    </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
